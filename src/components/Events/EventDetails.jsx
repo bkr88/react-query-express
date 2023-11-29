7e8907a3582fda1b978b41269932a4a29b98973f
antalbokor <antalbokor@gmail.com>
2023-11-29 20:47:30 +0100

    react-query-xpress useQuery and useMutation

---
+++ src/components/Events/EventDetails.jsx
@@ -0,0 +1,108 @@
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+
+import Header from '../Header.jsx';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+
+export default function EventDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['event', { id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+
+  const {
+    mutate,
+    isPending: isPendingSubmit,
+    isError: isErrorSubmit,
+    error: errorSubmit,
+  } = useMutation({
+    mutationFn: () => deleteEvent({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none',
+      });
+      navigate('/events');
+    },
+  });
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <p>Fetching event data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <ErrorBlock
+          title='Failed to load event'
+          message={
+            error.info?.message ||
+            'Failed to fetch event data, please try again later.'
+          }
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            {isPendingSubmit && 'Deleting...'}
+            {!isPendingSubmit && (
+              <button onClick={() => mutate({ event: id })}>Delete</button>
+            )}
+            <Link to='edit'>Edit</Link>
+          </nav>
+        </header>
+        <div id='event-details-content'>
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id='event-details-info'>
+            <div>
+              <p id='event-details-location'>{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {data.date} @ {data.time}
+              </time>
+            </div>
+            <p id='event-details-description'>{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Outlet />
+      <Header>
+        <Link to='/events' className='nav-item'>
+          View all Events
+        </Link>
+      </Header>
+      <article id='event-details'>
+        {content}
+        {isErrorSubmit && (
+          <ErrorBlock
+            title='Failed to create event'
+            message={
+              errorSubmit.info?.message ||
+              'Failed to create event. Please check your inputs and try again later'
+            }
+          />
+        )}
+      </article>
+    </>
+  );
+}
